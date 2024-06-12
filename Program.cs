676a6deb8c13e40d2fc93e0d14d2c2c34bf75371
monitz <igorb.kuhl@gmail.com>
2024-06-12 19:46:56 -0300

    Generates the random password.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace PasswordGenerator;
+
+class Program
+{
+    private static string _password = "";
+    private static List<string> _usedPasswords = new List<string>();
+
+    static void Main(string[] args)
+    {
+        string fileName = "used-passwords.txt";
+        _usedPasswords = File.ReadAllLines("./" + fileName).ToList();
+
+        GeneratePassword(20);
+
+        File.WriteAllLines("./" + fileName, _usedPasswords.ToArray());
+
+        Console.WriteLine("Your new password is:");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(_password);
+        Console.ResetColor();
+
+    }
+
+    private static void GeneratePassword(int passwordLength = 15)
+    {
+        StringBuilder password = new StringBuilder("", passwordLength);
+        string characters = "qwfpgjluyarstdhneiozxcvbkmQWFPGJUYARSTDHNEIOZXC" +
+            "VBKMçÇ@$#%^&*()_!+=-1234567890áàâäãúüöóõíéñÄÃÂÀÁÚÜÖÓÕÍÉÑ`€";
+        Random random = new Random();
+
+        for (int i = 0; i < passwordLength; i++)
+        {
+            int characterIndex = random.Next(0, characters.Length);
+            password.Append(characters[characterIndex]);
+        }
+        
+        _password = password.ToString();
+        _usedPasswords.Add(password.ToString());
+    }
+}
